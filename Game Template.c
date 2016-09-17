fee7fe47d14bb2fc2f23ad770bb4efa5c376347c
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-09-16 23:44:55 -0400

    Tested grass_create instruction

--- Game Template.c
+++ Game Template.c
@@ -26,45 +26,8 @@ void runGameScript()
 
 void initGameData()
 {
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //Temporary Test code for loading a simple polygon and box
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    	srand((unsigned)time(NULL));
-
-    	int boxWidth    = 8;
-    	int boxHeight   = 5;
-
-        polygon[0].radius = 2.0;
-        polygon[0].properties.mass = polygon[0].radius;
-		polygon[0].centre.xPosition = rand() % (int)(xMapSize - (2 * polygon[0].radius)) + (polygon[0].radius);
-		polygon[0].centre.yPosition = rand() % (int)(yMapSize - (2 * polygon[0].radius)) + (polygon[0].radius);
-		polygon[0].properties.colour[RED] = 255;
-		polygon[0].properties.colour[GREEN] = 255;
-		polygon[0].properties.colour[BLUE] = 255;
-
-		for (j = 0; j < MAX_POLYGON_SIDES; j++)
-		{
-			polygon[0].vertices[j].xPosition = polygon[0].centre.xPosition +
-				(polygon[0].radius * cos(((360 / MAX_POLYGON_SIDES) * (j)) * (PI / 180)));
-			polygon[0].vertices[j].yPosition = polygon[0].centre.yPosition +
-				(polygon[0].radius * sin(((360 / MAX_POLYGON_SIDES) * (j)) * (PI / 180)));
-		}
-
-		box[0].centre.xPosition = rand() % (int)(xMapSize - (boxWidth)) + (boxWidth);
-		box[0].centre.yPosition = rand() % (int)(yMapSize - (boxHeight)) + (boxHeight);
-		box[0].properties.colour[RED] = 255;
-		box[0].properties.colour[GREEN] = 255;
-		box[0].properties.colour[BLUE] = 255;
-
-        for (j = 0; j < 4; j++)
-		{
-			box[0].vertices[j].xPosition = box[0].centre.xPosition + ((boxWidth / 2)
-				  * sqrt(2) * (cos((45 + (j * 90)) * (PI / 180))));
-            box[0].vertices[j].yPosition = box[0].centre.yPosition + ((boxHeight / 2)
-				 * sqrt(2) * (sin((45 + (j * 90)) * (PI / 180))));
-		}
-		/////////////////////////////////////////////////////////////////////////////////////////////////////
+        grass_create(POLYGON, ENTITY, 3.0, 0, 0, 0, 0, 255, 255, 255);
+        grass_create(BOX, ENTITY, 0, 6.0, 4.0, 0, 0, 255, 255, 255);
 }
 
 //Controller
