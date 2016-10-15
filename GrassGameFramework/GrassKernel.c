6a290af025fce2f05eb0033b92ddf6c168870a27
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-15 16:33:41 -0400

    Separates GrassKernel

---
+++ GrassGameFramework/GrassKernel.c
@@ -0,0 +1,26 @@
+#include "Definitions.h"
+
+void runGrassKernel()
+{
+    compute_incrementTime();
+    frameCount++;
+
+    if(frameCount == 1)
+        initGameData();
+
+    readInput();
+
+    if(!gamePause)
+        runGameScript();
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //enables alpha blending with glColor4ub().
+
+    render_drawBox();
+    render_drawPolygon();
+    //drawGrid();
+    render_HUD();
+
+    glutSwapBuffers(); //freeglut buffer swap for animation.
+}
