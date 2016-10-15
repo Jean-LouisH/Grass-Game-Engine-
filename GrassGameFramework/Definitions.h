6dba9e32dc95bca71f5113eb52a8056e8d30a5ab
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-15 16:36:43 -0400

    Remove the compute smaller dimension due to non use.

--- GrassGameFramework/Definitions.h
+++ GrassGameFramework/Definitions.h
@@ -13,15 +13,14 @@
 //Define framework constants
 /////////////////////////////////////////////////////////
 
-#define VERSION                 "0.5.2"
+#define VERSION                 "0.6.5"
 
 //Physics
-#define PEAK_COLOUR_LEVEL       255.0
 #define PI                      3.1415926535897932384626433832795
 
 //Supported maximums
 #define MAX_POLYGONS            50
-#define MAX_POLYGON_SIDES       8
+#define MAX_POLYGON_SIDES       12
 #define MAX_BOXES               50
 
 #define FRAME_DELAY_MILLISECS   17
@@ -66,7 +65,14 @@ typedef struct
 
 typedef struct
 {
+    double width;
+    double height;
+}Rect;
+
+typedef struct
+{
     Vertex target;
+    Rect viewport;
 }Camera;
 
 typedef struct
@@ -103,19 +109,16 @@ typedef struct
 //Externs
 ////////////////////////////////////////////////////////////
 //Custom defined
-extern char keyStates[256];
+extern char keyStates[256]; //keyboard input buffer
 extern char gameTitle[64];
-extern int screenWidth;
-extern int screenHeight;
 extern double dpadSensitivity;
-extern double xMapSize;
-extern double yMapSize;
+
+extern Rect worldMap;
 extern bool gamePause;
 
-extern double friction;
+extern double friction; //for bounce calculations
 extern double objectGravity;
 extern double platformGravity;
-extern double pointGravity[8];
 
 //Loop iterations
 extern int i;
@@ -124,7 +127,6 @@ extern int k;
 
 extern int frameCount;
 extern double timeCount;
-extern double smallerMapDimension;
 
 extern RadialPolygon polygon[MAX_POLYGONS];
 extern Box box[MAX_BOXES];
