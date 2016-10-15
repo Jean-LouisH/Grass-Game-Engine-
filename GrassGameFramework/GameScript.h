1ade6592521984f16330124ba0d7f4fc41503a5f
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-15 16:37:52 -0400

    15.10.16 commentary

--- GrassGameFramework/GameScript.h
+++ GrassGameFramework/GameScript.h
@@ -1,4 +1,4 @@
 //Custom game scripts
-void initGameData();
-void runGameScript();
-void readInput();
+void initGameData(); //sets up all the game assets before running script.
+void runGameScript(); //contains game or simulation logic.
+void readInput(); //performs actions based on keyboard inputs in buffer.
