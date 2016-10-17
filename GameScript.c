2d981d94c43c8376a991e54f036185fd9ac48163
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-17 12:21:36 -0400

    GameScript now in template form.

--- GameScript.c
+++ GameScript.c
@@ -5,13 +5,12 @@
 #include "Suprannua\Compute.h" //All algorithms and physics calculations.
 #include "Suprannua\Editor.h" //Functions used for data manipulation in game script.
 
-
-///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 //Custom code for Game Data, Logic, AI, Inputs
-///////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
 //Global variables
-char gameTitle[64]          = SOFTWARE VERSION "[Alternation Test Script]";
+char gameTitle[64]          =  "";
 double dpadSensitivity      = 30;
 double cameraScrollSpeed    = 0.5;
 
@@ -20,27 +19,17 @@ Rect worldMap               = {100,100};
 double friction             = 0.4;
 double platformGravity      = 50.0;
 
-bool gamePause              = false;
+bool isGamePaused              = false;
 
 
 void initGameData()
 {
-    camera_target(worldMap.width / 2, worldMap.height / 2);
-    camera_resolution(worldMap.width);
 
-    edit_create(POLYGON, ENTITY, 5, 25.0, 0, 0, worldMap.width /2, worldMap.height / 2, 255, 255, 255);
 }
 
 void runGameScript()
 {
-    double maxRotation  = 3.0;
-    double rotation     = 0.0;
-    double duration     = 5.0;
 
-    compute_rotate();
-
-    rotation = maxRotation * sin((timeCount / duration) * (2 * PI));
-    AI_spin(POLYGON, 0, CLOCKWISE, rotation);
 }
 
 //Controller
@@ -61,13 +50,5 @@ void readInput()
         if (keyStates['d'] || keyStates['D'])
             ;
     }
-
-    /////////////
-    //Game states
-    /////////////
-
-    if (keyStates[27])
-        exit(EXIT_SUCCESS);
-
 }
 
