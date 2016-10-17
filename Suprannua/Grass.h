af6c8433f4a9d58b401072fedce67246499c4c86
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-17 12:26:10 -0400

    17.10.16

--- Suprannua/Grass.h
+++
@@ -1,6 +0,0 @@
-#include "Definitions.h" //All the enums, structs and variables the framework and script needs to know.
-#include "GameScript.h" //Script and asset functions.
-#include "2DCamera.h" //Camera functions.
-#include "AI.h" //Artificial Intelligence of agents.
-#include "Compute.h" //All algorithms and physics calculations.
-#include "Editor.h" //Functions used for data manipulation in game script.
