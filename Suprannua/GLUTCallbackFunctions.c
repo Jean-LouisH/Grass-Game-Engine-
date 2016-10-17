ea6d009206904bbd6e7c48b7dca7711e56c2e386
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-17 12:25:59 -0400

    Added ESC key to framework.

--- Suprannua/GLUTCallbackFunctions.c
+++ Suprannua/GLUTCallbackFunctions.c
@@ -13,15 +13,18 @@ void timer(int milliseconds)
 void keyPressed(unsigned char key, int x, int y)
 {
     keyStates[key] = true; //registers a button press in the input buffer.
+
+    if (keyStates[27]) //Escape key
+        exit(EXIT_SUCCESS);
 }
 
 void keyUp(unsigned char key, int x, int y)
 {
-    if((keyStates['p'] || keyStates['P']) && (!gamePause))
-        gamePause = true;
+    if((keyStates['p'] || keyStates['P']) && (!isGamePaused))
+        isGamePaused = true;
 
-    else if((keyStates['p'] || keyStates['P'] ) && (gamePause))
-        gamePause = false;
+    else if((keyStates['p'] || keyStates['P'] ) && (isGamePaused))
+        isGamePaused = false;
 
     keyStates[key] = false;
     polygon[0].properties.xVelocity = 0; //stops player movement immediately after releasing.
