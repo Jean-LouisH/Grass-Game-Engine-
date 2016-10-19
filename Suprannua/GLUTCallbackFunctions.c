d099be1e125606d74de5272c12d3f7574ca71d7f
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-10-18 22:57:33 -0400

    Special keys and window resizing enabled through GLUT callback

--- Suprannua/GLUTCallbackFunctions.c
+++ Suprannua/GLUTCallbackFunctions.c
@@ -16,6 +16,10 @@ void keyPressed(unsigned char key, int x, int y)
 
     if (keyStates[27]) //Escape key
         exit(EXIT_SUCCESS);
+
+    if(keyStates[9])
+        glutPositionWindow(((glutGet(GLUT_SCREEN_WIDTH) * 0.250)/ 2) + 1,
+                           ((glutGet(GLUT_SCREEN_HEIGHT) * 0.250 * 0.563) / 2) + 22);
 }
 
 void keyUp(unsigned char key, int x, int y)
@@ -28,5 +32,22 @@ void keyUp(unsigned char key, int x, int y)
 
     keyStates[key] = false;
     polygon[0].properties.xVelocity = 0; //stops player movement immediately after releasing.
+                                        //Considering an alternative method for custom freedom.
+}
+
+void keySpecial(unsigned char key, int x, int y)
+{
+    specialKeyStates[key] = true;
+}
+
+void keyUpSpecial(unsigned char key, int x, int y)
+{
+    specialKeyStates[key] = false;
+    polygon[0].properties.xVelocity = 0; //Considering an alternative method for custom freedom.
+}
+
+void resize(int width, int height)
+{
+    glutReshapeWindow(glutGet(GLUT_SCREEN_WIDTH) * 0.750, (glutGet(GLUT_SCREEN_WIDTH) * 0.750) * 0.563);
 }
 
--- Suprannua/GLUTCallbackFunctions.h
+++ Suprannua/GLUTCallbackFunctions.h
@@ -2,3 +2,6 @@
 void timer(int milliseconds); //for frame rate.
 void keyPressed(unsigned char key, int x, int y); //during key press.
 void keyUp(unsigned char key, int x, int y); //after key press.
+void keySpecial(unsigned char key, int x, int y);
+void keyUpSpecial(unsigned char key, int x, int y);
+void resize(int width, int height);
