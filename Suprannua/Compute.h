b4cab48e850460134336e1808933dcddb5729c3f
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-14 14:47:15 -0500

    Combined rotate and translate functions into transform

--- Suprannua/Compute.h
+++ Suprannua/Compute.h
@@ -9,7 +9,6 @@ void compute_limitBoundary();//prevents objects from escaping map dimensions.
 void compute_rigidBodyDynamics();//computes rag doll effects based on objects angle of edge from platform surface.
 void detectRadialCollision();//bounces radial polygons from each other
 void compute_detectPlatformCollision();//allows objects to settle on platforms or bounce from under.
-void compute_rotate();//update points based on angular displacement.
-void compute_translate(); //update points based on velocity.
+void compute_transform(); //update points based on velocity.
 void compute_gravitate(unsigned char object, int objectNumber, bool direction);//causes an object to fall.
 void compute_incrementTime();//updates game or simulation time.
