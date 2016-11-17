94f893b0ba622e5593de57da23bed36aad6aa814
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-17 13:00:47 -0500

    Handles kernelStats update and creating new text separately (experimental)

--- Suprannua/HUD.c
+++ Suprannua/HUD.c
@@ -1,4 +1,23 @@
 #include "Definitions.h"
 #include "HUD.h"
 
+void HUD_updateKernelStats()
+{
+    sprintf(textCache[0].text, SOFTWARE VERSION "Kernel time: %.0fs, Runtime: %.2fs, Frame count: %d, FPS: %.2f",
+            kernelTime, timeCount, frameCount, framesPerSecond);
+}
 
+void HUD_setText(double xText, double yText, char newText)
+{
+    int i;
+    textCache[1].text[0] = newText;
+    //sprintf(textCache[1].text, "%c", newText);
+    textCache[1].classification = CAMERA;
+
+}
+
+void HUD_removeText()
+{
+
+
+}
--- Suprannua/HUD.h
+++ Suprannua/HUD.h
@@ -1,1 +1,5 @@
+#include "Definitions.h"
 
+void HUD_updateKernelStats();
+void HUD_setText(double xText, double yText, char newText);
+void HUD_removeText();
