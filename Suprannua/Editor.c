9f3f11abdf3ada49042eac6ee6a7556e4e76a240
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-19 21:11:48 -0500

    Separates the polygon and block creator functions, added colour from text for simplicity.

--- Suprannua/Editor.c
+++ Suprannua/Editor.c
@@ -72,14 +72,76 @@ void edit_create(unsigned char object, unsigned char type, int numberOfSides,
                             block[i].properties.colour[BLUE] = blue;
                             block[i].properties.colour[ALPHA] = alpha;
 
-                            geometry_plotBox(i);
+                            geometry_plotBlock(i);
                         }
         break;
     }
 }
+void edit_createBlock(unsigned char type, double left, double right,
+                         double down, double up, unsigned char colour)
+{
+    int i;
+
+    for(i = 0; i < MAX_BLOCKS; i++)
+    {
+        if(block[i].properties.classification == NOTHING)
+            break;
+    }
+
+    switch(type)
+    {
+        case PLATFORM:      block[i].properties.classification = PLATFORM;      break;
+        case ENTITY:        block[i].properties.classification = ENTITY;        break;
+        case BACKGROUND:
+        default:            block[i].properties.classification = BACKGROUND;
+    }
+    block[i].dimensions.width = (right - left);
+    block[i].dimensions.height = (up - down);
+    block[i].centre.xPosition = (left + right) / 2;
+    block[i].centre.yPosition = (down + up) / 2;
+
+    edit_colourBlock(i, colour);
+    geometry_plotBlock(i);
+}
+void edit_createPolygon(unsigned char type, int numberOfSides, double newRadius, double newXPosition,
+                         double newYPosition, unsigned char colour)
+{
+    int i;
+
+    for(i = 0; i < MAX_POLYGONS; i++)
+    {
+        if(polygon[i].properties.classification == NOTHING)
+            break;
+    }
+
+    switch(type)
+    {
+        case PLATFORM:      polygon[i].properties.classification = PLATFORM;      break;
+        case ENTITY:        polygon[i].properties.classification = ENTITY;        break;
+        case BACKGROUND:
+        default:            polygon[i].properties.classification = BACKGROUND;
+    }
+    polygon[i].properties.edges = numberOfSides;
+    polygon[i].radius = newRadius;
+
+    if(newXPosition == 0 || newYPosition == 0) //provides a random position if non is specified.
+    {
+        polygon[i].centre.xPosition = rand() %
+        (int)(worldSizeMetres.width - (2 * polygon[i].radius) * (i + 1))+ (polygon[i].radius);
+        polygon[i].centre.yPosition = rand() %
+        (int)(worldSizeMetres.height - (2 * polygon[i].radius) * (i + 1)) + (polygon[i].radius);
+    }
+    else
+    {
+        polygon[i].centre.xPosition = newXPosition;
+        polygon[i].centre.yPosition = newYPosition;
+    }
+
+    edit_colourPolygon(i, colour);
+    geometry_plotPolygon(i);
+}
 void edit_remove(unsigned char object, int objectNumber)
 {
-
     int i;
 
     switch(object)
@@ -131,7 +193,7 @@ void edit_move(unsigned char object, int objectNumber, double newXPosition, doub
         case BLOCK:       block[objectNumber].centre.xPosition = newXPosition;
                         block[objectNumber].centre.yPosition = newYPosition;
 
-                        geometry_plotBox(objectNumber);
+                        geometry_plotBlock(objectNumber);
         break;
     }
 }
@@ -150,7 +212,7 @@ void edit_resize(unsigned char object, int objectNumber, double scale)
                         block[objectNumber].dimensions.width = block[objectNumber].dimensions.width * scale;
                         block[objectNumber].dimensions.height = block[objectNumber].dimensions.height * scale;
 
-                        geometry_plotBox(i);
+                        geometry_plotBlock(i);
         break;
     }
 }
@@ -216,8 +278,8 @@ void edit_adjust(unsigned char object, int objectNumber, unsigned char attribute
     }
 }
 
-void edit_colour(unsigned char object, int objectNumber, unsigned char red,
-                 unsigned char green, unsigned char blue, unsigned char alpha)
+void edit_colourFromRGBA(unsigned char object, int objectNumber, unsigned char red,
+                        unsigned char green, unsigned char blue, unsigned char alpha)
 {
     switch(object)
     {
@@ -231,6 +293,184 @@ void edit_colour(unsigned char object, int objectNumber, unsigned char red,
                         block[objectNumber].properties.colour[BLUE]   = blue;
                         block[objectNumber].properties.colour[ALPHA]  = alpha;
         break;
+    }
+}
+
+void edit_colourFromText(unsigned char object, int objectNumber, unsigned char colour)
+{
+    if(object == POLYGON)
+        edit_colourPolygon(objectNumber, colour);
+    else if (object == BLOCK)
+        edit_colourBlock(objectNumber, colour);
+}
+void edit_colourPolygon(int objectNumber, unsigned char colour)
+{
+    switch(colour)
+    {
+        case BLACK: polygon[objectNumber].properties.colour[RED]     = black[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = black[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = black[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case WHITE: polygon[objectNumber].properties.colour[RED]     = white[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = white[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = white[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case RED:   polygon[objectNumber].properties.colour[RED]     = red[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = red[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = red[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case GREEN: polygon[objectNumber].properties.colour[RED]     = green[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = green[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = green[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case BLUE:  polygon[objectNumber].properties.colour[RED]     = blue[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = blue[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = blue[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case ORANGE: polygon[objectNumber].properties.colour[RED]    = orange[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = orange[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = orange[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case YELLOW: polygon[objectNumber].properties.colour[RED]    = yellow[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = yellow[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = yellow[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case VIOLET: polygon[objectNumber].properties.colour[RED]    = violet[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = violet[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = violet[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case PURPLE: polygon[objectNumber].properties.colour[RED]    = purple[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = purple[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = purple[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case BROWN: polygon[objectNumber].properties.colour[RED]     = brown[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = brown[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = brown[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case SKY_BLUE: polygon[objectNumber].properties.colour[RED]  = skyBlue[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = skyBlue[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = skyBlue[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case GOLD: polygon[objectNumber].properties.colour[RED]      = gold[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = gold[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = gold[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case SEA_GREEN: polygon[objectNumber].properties.colour[RED] = seaGreen[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = seaGreen[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = seaGreen[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case PINK: polygon[objectNumber].properties.colour[RED]      = pink[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = pink[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = pink[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case GREY:  polygon[objectNumber].properties.colour[RED]     = grey[RED];
+                    polygon[objectNumber].properties.colour[GREEN]   = grey[GREEN];
+                    polygon[objectNumber].properties.colour[BLUE]    = grey[BLUE];
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        default:    polygon[objectNumber].properties.colour[RED]     = 0;
+                    polygon[objectNumber].properties.colour[GREEN]   = 0;
+                    polygon[objectNumber].properties.colour[BLUE]    = 0;
+                    polygon[objectNumber].properties.colour[ALPHA]   = FULL;
+    }
+}
+void edit_colourBlock(int objectNumber, unsigned char colour)
+{
+    switch(colour)
+    {
+        case BLACK: block[objectNumber].properties.colour[RED]     = black[RED];
+                    block[objectNumber].properties.colour[GREEN]   = black[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = black[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case WHITE: block[objectNumber].properties.colour[RED]     = white[RED];
+                    block[objectNumber].properties.colour[GREEN]   = white[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = white[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case RED:   block[objectNumber].properties.colour[RED]     = red[RED];
+                    block[objectNumber].properties.colour[GREEN]   = red[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = red[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case GREEN: block[objectNumber].properties.colour[RED]     = green[RED];
+                    block[objectNumber].properties.colour[GREEN]   = green[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = green[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case BLUE:  block[objectNumber].properties.colour[RED]     = blue[RED];
+                    block[objectNumber].properties.colour[GREEN]   = blue[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = blue[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case ORANGE: block[objectNumber].properties.colour[RED]    = orange[RED];
+                    block[objectNumber].properties.colour[GREEN]   = orange[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = orange[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case YELLOW: block[objectNumber].properties.colour[RED]    = yellow[RED];
+                    block[objectNumber].properties.colour[GREEN]   = yellow[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = yellow[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case VIOLET: block[objectNumber].properties.colour[RED]    = violet[RED];
+                    block[objectNumber].properties.colour[GREEN]   = violet[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = violet[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case PURPLE: block[objectNumber].properties.colour[RED]    = purple[RED];
+                    block[objectNumber].properties.colour[GREEN]   = purple[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = purple[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case BROWN: block[objectNumber].properties.colour[RED]     = brown[RED];
+                    block[objectNumber].properties.colour[GREEN]   = brown[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = brown[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case SKY_BLUE: block[objectNumber].properties.colour[RED]  = skyBlue[RED];
+                    block[objectNumber].properties.colour[GREEN]   = skyBlue[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = skyBlue[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case GOLD: block[objectNumber].properties.colour[RED]      = gold[RED];
+                    block[objectNumber].properties.colour[GREEN]   = gold[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = gold[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case SEA_GREEN: block[objectNumber].properties.colour[RED] = seaGreen[RED];
+                    block[objectNumber].properties.colour[GREEN]   = seaGreen[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = seaGreen[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case PINK: block[objectNumber].properties.colour[RED]      = pink[RED];
+                    block[objectNumber].properties.colour[GREEN]   = pink[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = pink[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        case GREY:  block[objectNumber].properties.colour[RED]     = grey[RED];
+                    block[objectNumber].properties.colour[GREEN]   = grey[GREEN];
+                    block[objectNumber].properties.colour[BLUE]    = grey[BLUE];
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
+        break;
+        default:    block[objectNumber].properties.colour[RED]     = 0;
+                    block[objectNumber].properties.colour[GREEN]   = 0;
+                    block[objectNumber].properties.colour[BLUE]    = 0;
+                    block[objectNumber].properties.colour[ALPHA]   = FULL;
     }
 }
 void edit_force(unsigned char firstObject, int firstObjectNumber,
--- Suprannua/Editor.h
+++ Suprannua/Editor.h
@@ -2,6 +2,10 @@
 void edit_create(unsigned char object, unsigned char type, int numberOfSides, double newRadius, double newWidth,
        double newHeight, double newXPosition, double newYPosition,
        unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha); //object; "polygon", "box" | type; BACKGROUND, ENTITY, PLATFORM |
+void edit_createBlock(unsigned char type, double left, double right,
+                         double down, double up, unsigned char colour);
+void edit_createPolygon(unsigned char type, int numberOfSides, double newRadius, double newXPosition,
+                         double newYPosition, unsigned char colour);
 void edit_remove(unsigned char object, int objectNumber);
 void edit_move(unsigned char object, int objectNumber, double newXPosition, double newYPosition);
 void edit_resize(unsigned char object, int objectNumber, double scale);
@@ -9,7 +13,8 @@ void edit_change(unsigned char object, int objectNumber, unsigned char attribute
             //attributes; "angle", "mass", "xVelocity", "yVelocity", "gravity", "friction"
             //GAME object allows access to gravity and friction.
 void edit_adjust(unsigned char object, int objectNumber, unsigned char attribute, double amount);
-void edit_colour(unsigned char object, int objectNumber, unsigned char red,
+void edit_colourFromRGBA(unsigned char object, int objectNumber, unsigned char red,
                  unsigned char green, unsigned char blue, unsigned char alpha);
+void edit_colourFromText(unsigned char object, int objectNumber, unsigned char colour);
 void edit_force(unsigned char firstObject, int firstObjectNumber,
                  unsigned char preposition, unsigned char secondObject, int secondobjectNumber);// preposition - "to", "from"
