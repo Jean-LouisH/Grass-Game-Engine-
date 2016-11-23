554b8cc5e12d2246ff59047bf5da404bf91ba380
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-22 23:44:10 -0500

    Added Polygon contact functions for left and right sides of platforms.

--- Suprannua/Logic.h
+++ Suprannua/Logic.h
@@ -1,4 +1,7 @@
 bool logic_isOnPlatform(unsigned char object, int objectNumber, int platformNumber);
+bool logic_isWithinPlatformHeight(unsigned char object, int objectNumber, int platformNumber);
 bool logic_isTouchingUnderPlatform(unsigned char object, int objectNumber, int platformNumber);
 bool logic_isWithinPlatformWidth(unsigned char object, int objectNumber, int platformNumber);
+bool logic_isTouchingLeftOfPlatform(unsigned char object, int objectNumber, int platformNumber);
+bool logic_isTouchingRightOfPlatform(unsigned char object, int objectNumber, int platformNumber);
 bool logic_arePolygonsTouching(int firstObjectNumber, int secondObjectNumber);
