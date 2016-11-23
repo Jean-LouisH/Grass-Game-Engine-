e9b1ef28ecc571aa65f5259ffafedb0b452e9ac4
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-22 23:41:06 -0500

    Added Polygon contact functions for left and right sides of platforms.

--- Suprannua/Logic.c
+++ Suprannua/Logic.c
@@ -15,6 +15,21 @@ bool logic_isWithinPlatformWidth(unsigned char object, int objectNumber, int pla
     return (platformFlag);
 }
 
+bool logic_isWithinPlatformHeight(unsigned char object, int objectNumber, int platformNumber)
+{
+    bool platformFlag = false;
+
+    if(polygon[objectNumber].centre.yPosition >
+            block[platformNumber].centre.yPosition - (block[platformNumber].dimensions.height / 2) &&
+        polygon[objectNumber].centre.yPosition <
+            block[platformNumber].centre.yPosition + (block[platformNumber].dimensions.height / 2))
+    {
+        platformFlag = true;
+    }
+
+    return (platformFlag);
+}
+
 bool logic_isOnPlatform(unsigned char object, int objectNumber, int platformNumber)
 {
     bool platformFlag = false;
@@ -61,6 +76,54 @@ bool logic_isTouchingUnderPlatform(unsigned char object, int objectNumber, int p
                             }
                         }
         break;
+    }
+
+    return (platformFlag);
+}
+
+bool logic_isTouchingLeftOfPlatform(unsigned char object, int objectNumber, int platformNumber)
+{
+    bool platformFlag = false;
+
+    if(object == POLYGON)
+    {
+        if(block[platformNumber].properties.classification == PLATFORM)
+        {
+            if(polygon[objectNumber].centre.xPosition + polygon[objectNumber].radius >=
+                    block[platformNumber].centre.xPosition - (block[platformNumber].dimensions.width / 2) &&
+                polygon[objectNumber].centre.xPosition + polygon[objectNumber].radius <
+                    block[platformNumber].centre.xPosition + (block[platformNumber].dimensions.width / 2))
+            {
+                if(logic_isWithinPlatformHeight(object, objectNumber, platformNumber))
+                {
+                    platformFlag = true;
+                }
+            }
+        }
+    }
+
+    return (platformFlag);
+}
+
+bool logic_isTouchingRightOfPlatform(unsigned char object, int objectNumber, int platformNumber)
+{
+    bool platformFlag = false;
+
+    if(object == POLYGON)
+    {
+        if(block[platformNumber].properties.classification == PLATFORM)
+        {
+            if(polygon[objectNumber].centre.xPosition - polygon[objectNumber].radius <=
+                    block[platformNumber].centre.xPosition + (block[platformNumber].dimensions.width / 2) &&
+                polygon[objectNumber].centre.xPosition - polygon[objectNumber].radius >
+                    block[platformNumber].centre.xPosition - (block[platformNumber].dimensions.width / 2))
+            {
+                if(logic_isWithinPlatformHeight(object, objectNumber, platformNumber))
+                {
+                    platformFlag = true;
+                }
+            }
+        }
     }
 
     return (platformFlag);
