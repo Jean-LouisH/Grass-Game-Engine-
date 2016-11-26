722ca80f5d48bd7ba43f85f49cb0e223d9b78197
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-25 23:30:08 -0500

    Adds targeted AI function implementations.

--- Suprannua/AI.c
+++ Suprannua/AI.c
@@ -9,3 +9,43 @@ void AI_spin(unsigned char agent, int agentNumber, bool direction, double amount
         break;
     }
 }
+
+void AI_follow(unsigned char agent, int agentNumber, unsigned char object, int objectNumber)
+{
+
+}
+
+void AI_avoid(unsigned char agent, int agentNumber, unsigned char object, int objectNumber)
+{
+
+}
+
+void AI_shoot(unsigned char agent, int agentNumber, unsigned char object, int objectNumber)
+{
+
+}
+
+void AI_mimic(unsigned char agent, int agentNumber, unsigned char object, int objectNumber)
+{
+
+}
+
+void AI_signal(unsigned char message)
+{
+
+}
+
+void AI_listen()
+{
+
+}
+
+void AI_catch(unsigned char agent, int agentNumber, unsigned char object, int objectNumber)
+{
+
+}
+
+void AI_travel(unsigned char agent, int agentNumber, double toXPosition, double toYPosition, double Velocity)
+{
+
+}
