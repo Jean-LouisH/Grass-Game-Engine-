cca0d980ce6f7fb46a7e841b30c6cdbbac98fce0
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-26 22:02:28 -0500

    Controls render_drawGrid with gridState selected by game.

--- Suprannua/Kernel.c
+++ Suprannua/Kernel.c
@@ -33,13 +33,16 @@ void runKernel()
 
     //HUD_updateKernelStats();
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //enables alpha blending with glColor4ub().
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //enables alpha blending with glColor4ub().
 
     render_displayBlockList();
     render_displayPolygonList();
-	//render_drawGrid();
+
+	if(gridState)
+		render_drawGrid();
+
     //render_postHUD();
 
     glutSwapBuffers(); //freeglut buffer swap for animation.
