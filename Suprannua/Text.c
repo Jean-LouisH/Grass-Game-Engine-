77c6ecb2d8b73d022c681d3a5bdb14ef6fc01ec4
Jean-LouisHaywood <LouisLou512.e@gmail.com>
2016-11-30 14:28:57 -0500

    Renamed HUD to Text to reflect all text objects from heads up display to map pinned text.

--- Suprannua/Text.c
+++ Suprannua/Text.c
@@ -1,21 +1,24 @@
 #include "Suprannua.h"
 
-void HUD_updateKernelStats()
+void text_updateKernelStats()
 {
+	/*
     sprintf(textCache[0].text, SOFTWARE VERSION "Kernel time: %.0fs, Runtime: %.2fs, Frame count: %d, FPS: %.2f",
             kernelTime, timeCount, frameCount, framesPerSecond);
+	*/
 }
 
-void HUD_setText(double xText, double yText, char newText)
+void text_set(double xText, double yText, char newText)
 {
+	/*
     int i;
     textCache[1].text[0] = newText;
     //sprintf(textCache[1].text, "%c", newText);
     textCache[1].classification = HUD;
-
+	*/
 }
 
-void HUD_removeText(int textCell)
+void text_remove(int textCell)
 {
 
 }
---
+++ Suprannua/Text.h
@@ -0,0 +1,11 @@
+/**
+ * Text
+ *
+ * Author: Jean-Louis Haywood
+ *
+ * Handles string data within the TextCache array to be positioned relative to the 2D camera or world.
+ */
+
+void text_updateKernelStats();								//Takes frame rate and time data, and then updates TextCache[0].
+void text_set(double xText, double yText, char newText);	//Fills an empty array cell with new text.
+void text_remove(int textCell);								//Releases an array cell with text.
