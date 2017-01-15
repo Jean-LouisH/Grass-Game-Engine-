e6b8610e63601ad8d820fada64dfdb3174eaab9f
Jean-LouisH <louis_ml@live.co.uk>
2017-01-15 02:19:12 -0500

    Added Agar.io in Suprannua as a game example.

---
+++ Game Samples/Agar.io in Suprannua.c
@@ -0,0 +1,228 @@
+#include "Suprannua Engine\SuprannuaEngine.h" 
+
+/*Made with Suprannua 0.12.0 higher versions are probably incompatible with this code as is.*/
+/*The aim is to collect as many smaller polygons until you burst to gain win points. If you are
+eaten, your losses are also scored. */
+
+/*Global variables*/
+
+char gameTitle[64]			= "Agar.io in Suprannua";
+Rect worldSizeMetres		= { 100,100 }; // m
+double dpadSensitivity		= 10.0; // m/s
+double cameraScrollSpeed	= 50.0; // m/s
+double platformGravity		= 9.8; // m/s^2
+double gravityConstant		= 6.674E-11; // m/s^2
+bool isGamePaused			= false;
+
+void initGame()
+{
+	int i;
+
+	camera_setViewportWidth(50.0);
+	camera_setTarget(edit_get(CAMERA, 0, XCENTRE), edit_get(CAMERA, 0, YCENTRE));
+	edit_createRectangle(BACKGROUND, 0, edit_get(GAME, 0, WIDTH), 0, edit_get(GAME, 0, HEIGHT), GREY);
+
+	/*Insert Game Initialisation code.*/
+	edit_enableGrid(true);
+	edit_createPolygon(ENTITY, 12, 2.0, edit_get(GAME, 0, XCENTRE) + 4, edit_get(GAME, 0, YCENTRE) + 4, DARK_GREEN);
+	for (i = 1; i <= 10; i++)
+		edit_createPolygon(ENTITY, 12, 2.0, 0, 0, RED);
+	for (i = 11; i <= 15; i++)
+		edit_createPolygon(BACKGROUND, 16, 0.5, 0, 0, BLUE);
+	text_set(ENTITY, edit_get(GAME, 0, XCENTRE), edit_get(GAME, 0, YCENTRE), "AGAR.IO IN SUPRANNUA", WHITE);
+	text_set(HUD, -1.0, 0.95, "", WHITE);
+	text_set(HUD, -0.5, 0.95, "", WHITE);
+	text_set(HUD, 0.0, 0.95, "WASD - move, P - pause. While paused: Q - Zoom out, E - Zoom in, X - Reset Camera", WHITE);
+	text_set(HUD, -0.25, -0.95, "Score wins by absorbing polygons until you burst.", WHITE);
+}
+
+/*Controls*/
+void readInput()
+{
+	int i;
+
+	if (gameState == GAMEPLAY)
+	{
+		/*physics_resistRolling to be updated for general motion on screen in Game Engine*/
+		for(i = 0; i <= storedPolygons; i++)
+		{
+			physics_resistRolling(POLYGON, i, dpadSensitivity * 2);
+
+			if (polygon[0].properties.yVelocity > 0)
+			{
+				edit_adjust(POLYGON, 0, YVELOCITY, (-1 * dpadSensitivity * 2));
+				if (polygon[0].properties.yVelocity < 0)
+					polygon[0].properties.yVelocity = 0;
+			}
+			else if (polygon[0].properties.yVelocity < 0)
+			{
+				edit_adjust(POLYGON, 0, YVELOCITY, (dpadSensitivity * 2));
+				if (polygon[0].properties.yVelocity > 0)
+					polygon[0].properties.yVelocity = 0;
+			}
+		}
+
+		if (input_isPressed('w'))
+		{
+			edit_change(POLYGON, 0, YVELOCITY, dpadSensitivity);
+		}
+
+		if (input_isPressed('a'))
+		{
+			edit_change(POLYGON, 0, XVELOCITY, -1 * dpadSensitivity);
+		}
+
+		if (input_isPressed('s'))
+		{
+			edit_change(POLYGON, 0, YVELOCITY, -1 * dpadSensitivity);
+		}
+
+		if (input_isPressed('d'))
+		{
+			edit_change(POLYGON, 0, XVELOCITY, dpadSensitivity);
+		}
+	}
+	else if (gameState == MENU)
+	{
+		if (input_isPressed('w'))
+		{
+			camera_scroll(0.0, cameraScrollSpeed);
+		}
+
+		if (input_isPressed('a'))
+		{
+			camera_scroll(-1 * cameraScrollSpeed, 0.0);
+		}
+
+		if (input_isPressed('s'))
+		{
+			camera_scroll(0.0, -1 * cameraScrollSpeed);
+		}
+
+		if (input_isPressed('d'))
+		{
+			camera_scroll(cameraScrollSpeed, 0.0);
+		}
+
+		if (input_isPressed('q'))
+		{
+			camera_keepZoomingBy(cameraScrollSpeed / 32);
+		}
+
+		if (input_isPressed('e'))
+		{
+			camera_keepZoomingBy(-cameraScrollSpeed / 32);
+		}
+
+		if (input_isPressed('x'))
+		{
+			camera_setTarget(edit_get(GAME, 0, XCENTRE), edit_get(GAME, 0, YCENTRE));
+			camera_setViewportWidth(edit_get(GAME, 0, WIDTH));
+		}
+	}
+}
+
+void runGame()
+{
+	int i;
+	int j;
+	double lowestDistance = edit_get(GAME, 0, WIDTH);
+	double currentDistance;
+	int target = 0;
+	double positionAngle;
+	double motion;
+	static int wins = 0;
+	static int losses = 0;
+
+	physics_limitBoundary();
+	//physics_detectPolygonCollision();
+	//physics_detectPlatformCollision();
+
+	/*Insert Game Script code.*/
+	camera_follow(POLYGON, 0, true, true);
+	text_update(3, "WINS");
+	text_data(3, wins);
+	text_update(4, "LOSSES");
+	text_data(4, losses);
+
+	/*AI following the nearest smaller polygon.*/
+	for (i = 1; i <= storedPolygons; i++)
+	{
+		if (polygon[i].properties.classification == ENTITY)
+		{
+			for (j = 0; j <= storedPolygons; j++)
+			{
+				if (i != j)
+				{
+					currentDistance = geometry_findDistance(POLYGON, i, POLYGON, j);
+					if ((currentDistance < lowestDistance) && (edit_get(POLYGON, j, RADIUS) < edit_get(POLYGON, i, RADIUS)))
+					{
+						lowestDistance = currentDistance;
+						target = j;
+					}
+				}
+			}
+
+			/*Movement AI to be added to Game Engine*/
+			motion = dpadSensitivity;
+
+			positionAngle = atan2(polygon[target].centre.yPosition - polygon[i].centre.yPosition,
+				polygon[target].centre.xPosition - polygon[i].centre.xPosition);
+
+			polygon[i].properties.xVelocity = motion * cos(positionAngle);
+			polygon[i].properties.yVelocity = motion * sin(positionAngle);
+
+			lowestDistance = edit_get(GAME, 0, WIDTH);
+			target = i;
+		}
+	}
+
+	/*Expands winning polygon and destroys losing polygon*/
+	for (i = 0; i <= storedPolygons; i++)
+	{
+		if (polygon[i].properties.classification == ENTITY)
+		{
+			for (j = 0; j <= storedPolygons; j++)
+			{
+				if (event_arePolygonsTouching(i, j) && (edit_get(POLYGON, j, RADIUS) < edit_get(POLYGON, i, RADIUS)))
+				{
+					polygon[i].radius = sqrt(((pow(polygon[i].radius, 2) * 2 * PI) + (pow(polygon[j].radius, 2) * 2 * PI)) / (2 * PI));
+					edit_remove(POLYGON, j);
+					if (j == 0)
+						losses++;
+				}
+			}
+		}
+	}
+
+	/*Generates polygons every 2 seconds. Event to be added to Game Engine*/
+	if (frameCount % (60 * 2) == (60 * 2 - 1))
+	{
+		for(i = 0; i < 30; i++)
+			edit_createPolygon(BACKGROUND, 16, 0.5, 0, 0, BLUE);
+	}
+
+	/*Converts blue polygons in player memory slots into players*/
+	for (i = 0; i <= 10; i++)
+	{
+		if (polygon[i].properties.colour[BLUE] == FULL)
+		{
+			polygon[i].properties.classification = ENTITY;
+			edit_change(POLYGON, i, RADIUS, 2.0);
+			if (i == 0)
+				edit_colourPolygon(i, DARK_GREEN);
+			else
+				edit_colourPolygon(i, RED);
+		}
+
+		/*When a player achieves the radius size, they burst*/
+		if (polygon[i].radius > 10)
+		{
+			for (j = 0; j < (10 * 2); j++)
+				edit_createPolygon(BACKGROUND, 12, 0.5, polygon[i].centre.xPosition + (j * 0.5), polygon[i].centre.yPosition - (j * 0.5), BLUE);
+			if (i == 0)
+				wins++;
+			edit_remove(POLYGON, i);
+		}
+	}
+}
