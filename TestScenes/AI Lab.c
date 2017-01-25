83caa2a43ba7adfb49a6e673d358c39e3ece1547
Jean-LouisH <louis_ml@live.co.uk>
2017-01-24 23:53:11 -0500

    Updated 24.01.17 for use of testing AI_fly()

--- TestScenes/AI Lab.c
+++ TestScenes/AI Lab.c
@@ -1,22 +1,22 @@
-#include "Suprannua\Suprannua.h" 
+#include "../Suprannua Engine/SuprannuaEngine.h" 
 
 /**
- *
- *				DOCUMENTATION IN PROGRESS...
- *
- */
+*
+*				DOCUMENTATION IN PROGRESS...
+*
+*/
 
 /*Custom code for Initialisation, Scripting and Controls*/
 
-//Global variables
+/*Global variables*/
 
-char gameTitle[64]			= SOFTWARE VERSION "[Empty Game.c Source File.]";
-Rect worldSizeMetres		= { 100,57 }; // m
-double dpadSensitivity		= 5.0;
-double cameraScrollSpeed	= 1.0;
-double platformGravity		= 9.8; // m/s^2
-double gravityConstant		= 6.674E-11; // m/s^2
-bool isGamePaused			= false;
+char gameTitle[64] = "AI Lab [Test Scene, " SOFTWARE VERSION "]";
+Rect worldSizeMetres = { 50,29 }; // m
+double dpadSensitivity = 10.0; // m/s
+double cameraScrollSpeed = 50.0; // m/s
+double platformGravity = 9.8; // m/s^2
+double gravityConstant = 6.674E-11; // m/s^2
+bool isGamePaused = false;
 
 
 void initGame()
@@ -25,22 +25,31 @@ void initGame()
 	camera_setTarget(edit_get(CAMERA, 0, XCENTRE), edit_get(CAMERA, 0, YCENTRE));
 	edit_createRectangle(BACKGROUND, 0, edit_get(GAME, 0, WIDTH), 0, edit_get(GAME, 0, HEIGHT), SKY_BLUE);
 
-	//Insert Game Initialisation code.
+	/*Insert Game Initialisation code.*/
+	edit_createRectangle(PLATFORM, 0, edit_get(GAME, 0, WIDTH), 0.0, 1.0, BROWN);
+	edit_createRectangle(PLATFORM, 0, edit_get(GAME, 0, WIDTH), 1.0, 1.25, DARK_GREEN);
+	edit_createRectangle(PLATFORM, 6.0, 12.0, 8.0, 8.25, DARK_GREEN);
+	edit_createRectangle(PLATFORM, 6.0 + 5, 12.0 + 5, 8.0 - 4, 8.25 - 4, DARK_GREEN);
+	edit_createPolygon(ENTITY, 8, 1.0, 3.0, 10.0, DARK_GREY);
+	edit_createPolygon(AIRBOURNE, 8, 1.0, 10.0, 10.0, WHITE);
 }
 
-//Controls
+/*Controls*/
 void readInput()
 {
 	if (gameState == GAMEPLAY)
 	{
+		physics_resistMovement(POLYGON, 0, LEFT_RIGHT, dpadSensitivity * 2);
+		physics_resistMovement(POLYGON, 1, LEFT_RIGHT, dpadSensitivity * 2);
+
 		if (input_isPressed('w'))
 		{
-			;
+			AI_jump(POLYGON, 0, 10.0);
 		}
 
 		if (input_isPressed('a'))
 		{
-			;
+			edit_change(POLYGON, 0, XVELOCITY, -1 * dpadSensitivity);
 		}
 
 		if (input_isPressed('s'))
@@ -50,7 +59,7 @@ void readInput()
 
 		if (input_isPressed('d'))
 		{
-			;
+			edit_change(POLYGON, 0, XVELOCITY, dpadSensitivity);
 		}
 	}
 	else if (gameState == MENU)
@@ -77,12 +86,12 @@ void readInput()
 
 		if (input_isPressed('q'))
 		{
-			camera_keepZoomingBy(cameraScrollSpeed);
+			camera_keepZoomingBy(cameraScrollSpeed / 32);
 		}
 
 		if (input_isPressed('e'))
 		{
-			camera_keepZoomingBy(-cameraScrollSpeed);
+			camera_keepZoomingBy(-cameraScrollSpeed / 32);
 		}
 
 		if (input_isPressed('x'))
@@ -99,5 +108,12 @@ void runGame()
 	physics_detectPolygonCollision();
 	physics_detectPlatformCollision();
 
-	//Insert Game Script code.
+	/*Insert Game Script code.*/
+	physics_gravitate(POLYGON, 0, DOWN);
+	physics_roll(POLYGON, 0);
+	physics_gravitate(POLYGON, 1, DOWN);
+	physics_roll(POLYGON, 1);
+
+	//AI_avoid(POLYGON, 1, POLYGON, 0, 7.0);
+	AI_fly(POLYGON, 1, 10.0);
 }
